fc2e24e2a59bc34e92c21f6c56e286f863fdc119
Robin Joshua Del Mundo <batbord94@gmail.com>
2022-04-07 23:33:46 +0800

    Added animations and functionalities

--- src/App.js
+++ src/App.js
@@ -16,21 +16,45 @@ function App() {
 export default App;
 
 class RatingsContainer extends React.Component{
+
   render(){
     return(
-      <div className='ratings-main-container'>
+      <div className='ratings-main-container' onClick={this.handleClick} >
         <Texts />
         <RatingsNumbers />
       </div>
     );
   }
+
 }
 
 class Texts extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      clicked: false
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+    this.setState({
+      clicked: true
+    });
+
+    setTimeout(() => {
+      this.setState({
+        clicked: false
+      })
+    }, 500);
+  }
+
   render(){
     return(
       <div className='texts-container'>
-        <div className='star-icon-container'>
+        <div className={'star-icon-container noselect' + (this.state.clicked === true ? ' clicked' : '')} onClick={this.handleClick}>
           <img src={iconStar} alt="Star Icon" />
         </div>
         <h1>How did we do?</h1>
@@ -87,10 +111,21 @@ ThankYou.defaultProps = {
 }
 
 class RatingsNumbers extends React.Component{
+  constructor(props){
+    super(props);
 
-  state = {
-    show: false,
-    rate: 0
+    this.state ={
+      show: false,
+      rate: 0,
+      clickedSubmit: false
+    }
+
+  }
+
+  handleClick(){
+    this.setState({
+      clickedSubmit: true
+    });
   }
 
   render(){
@@ -106,7 +141,8 @@ class RatingsNumbers extends React.Component{
       if (ratingSubmit > 0){
         this.setState({
           show: true,
-          rate: ratingSubmit
+          rate: ratingSubmit,
+          clickedSubmit: true
         });
       }
 
@@ -115,8 +151,7 @@ class RatingsNumbers extends React.Component{
     return(
       <div className="form-container">
         <form onSubmit={handleSubmit} action="#" className="ratings-form" id="ratingsForm" name="ratingsForm">
-          <Rating />
-          <button className="submit noselect" type="submit" value="submit">Submit</button>
+          <Rating clickedSubmit={this.state.clickedSubmit}/>
         </form>
         <ThankYou rating={this.state.rate} show={this.state.show}/>
       </div>
@@ -128,8 +163,12 @@ class Rating extends React.Component{
   constructor(props){
     super(props);
 
+    this.ratingInput = React.createRef();
+
     this.state = {
+      showNotif: false,
       activeRating: 0,
+      clicked: false,
       ratings: 
       [{
         id: 1,
@@ -154,31 +193,88 @@ class Rating extends React.Component{
     }
 
     this.handleClick = this.handleClick.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+    this.handleNotif = this.handleNotif.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handleClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleClickOutside);
+  }
+
+  handleClickOutside(event) {
+    if (this.ratingInput && !this.ratingInput.current.contains(event.target) && this.props.clickedSubmit !== true) {
+      this.setState({
+        activeRating: 0,
+        clicked: true
+      })
+    }
   }
 
   //get the current selected number's id to apply the class
   handleClick = id => {
     this.setState({
-      activeRating: id
+      activeRating: id,
+      clicked: false,
+      showNotif: false
     });
+  }
+
+  handleNotif(){
+
+    if (this.state.activeRating < 1){
+      this.setState({
+        showNotif: true
+      });
+
+      setTimeout(() => {
+        this.setState({
+          showNotif: false
+        })
+      }, 1500);
+    }
+
   }
  
   render(){
 
     return(
-      <div className="ratings-numbers">
-          {
-            this.state.ratings.map(rating => {
-              return (
-              <div key={rating.id} id={rating.id} onClick={() => (this.handleClick(rating.id) )} className={rating.className + (rating.id === this.state.activeRating ? " selected" : "")}>
-                <label>{rating.id}</label>
-              </div>
-              )
-            })
-          }
-          {/* this input element's value property is rendered with the id of the current selected div of this component to be able to give the input data into the form tag */}
-          <input type="hidden" id="ratingSubmission" name='ratingSubmission' value={this.state.activeRating} />
+      <div ref={this.ratingInput}>
+        <div className="ratings-numbers">
+            {
+              this.state.ratings.map(rating => {
+                return (
+                <div key={rating.id} id={rating.id} onClick={() => ( this.handleClick(rating.id) )} className={rating.className + (rating.id === this.state.activeRating && this.state.clicked === false ? " selected" : "")}>
+                  <label>{rating.id}</label>
+                </div>
+                )
+              })
+            }
+            <Notification isShown={this.state.showNotif}/>
+            {/* this input element's value property is rendered with the id of the current selected div of this component to be able to give the input data into the form tag */}
+            <input type="hidden" id="ratingSubmission" name='ratingSubmission' value={this.state.activeRating} />
+        </div>
+        <button className="submit noselect" type="submit" value="submit" onClick={this.handleNotif}>Submit</button>
       </div>
     )
   }
+}
+
+class Notification extends React.Component{
+
+  render(){
+    return(
+      <div className={'notification-wrapper' + (this.props.isShown === true ? ' is-shown' : '')}>
+        <div className="notification">
+          <p>Please consider selecting a rating first 😊</p>
+        </div>
+        <div className="pointer">
+        </div>
+      </div>
+    )
+  }
+
 }
